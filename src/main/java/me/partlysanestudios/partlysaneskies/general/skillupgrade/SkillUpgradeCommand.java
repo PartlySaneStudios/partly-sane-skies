2acd0190fc681fd3efee95fd817a8c8a5a99a6a4
Su386yt <83100266+Su386yt@users.noreply.github.com>
2022-09-06 09:15:30 -0700

    Added Skill Upgrade Command

---
+++ src/main/java/me/partlysanestudios/partlysaneskies/general/skillupgrade/SkillUpgradeCommand.java
@@ -0,0 +1,67 @@
+package me.partlysanestudios.partlysaneskies.general.skillupgrade;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import me.partlysanestudios.partlysaneskies.Main;
+import me.partlysanestudios.partlysaneskies.utils.Utils;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.BlockPos;
+
+public class SkillUpgradeCommand implements ICommand{
+    @Override
+    public int compareTo(ICommand o) {
+        return 0;
+    }
+
+    @Override
+    public String getCommandName() {
+        return "skillup";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return "/skillup [username]";
+    }
+
+    @Override
+    public List<String> getCommandAliases() {
+        return Arrays.asList("skillup", "skillu", "su");
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        if(args.length > 0) {
+            try {
+                SkillUpgradeRecommendation.getRecomendedSkills(args[0]);
+            } catch (IOException e) {
+                Utils.sendClientMessage(Utils.colorCodes("Error getting data for " + args[0] + ". Maybe the player is nicked or there is an invalid API key. Try running /api new."));
+            }
+        }
+        else { 
+            try {
+                SkillUpgradeRecommendation.getRecomendedSkills(Main.minecraft.thePlayer.getName());
+            } catch (IOException e) {
+                Utils.sendClientMessage(Utils.colorCodes("Error getting data for " + Main.minecraft.thePlayer.getName() + ". Maybe the player is nicked or there is an invalid API key. Try running /api new."));
+            }
+        }
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+        return null;
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return false;
+    }
+}
---
+++ src/main/java/me/partlysanestudios/partlysaneskies/general/skillupgrade/SkillUpgradeRecommendation.java
@@ -0,0 +1,110 @@
+package me.partlysanestudios.partlysaneskies.general.skillupgrade;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import me.partlysanestudios.partlysaneskies.utils.Utils;
+
+public class SkillUpgradeRecommendation {
+    public enum Skills {
+        MINING,
+        FORAGING,
+        ENCHANTING,
+        FARMING,
+        COMBAT,
+        FISHING,
+        ALCHEMY,
+        CATACOMBS
+    }
+
+    
+    public static int maxLevel(Skills skill) {
+        if(skill.equals(Skills.MINING)) return 60;
+        else if(skill.equals(Skills.FORAGING)) return 50;
+        else if(skill.equals(Skills.ENCHANTING)) return 60;
+        else if(skill.equals(Skills.COMBAT)) return 60;
+        else if(skill.equals(Skills.FARMING)) return 60;
+        else if(skill.equals(Skills.FISHING)) return 50;
+        else if(skill.equals(Skills.ALCHEMY)) return 50;
+        else if(skill.equals(Skills.CATACOMBS)) return 50;
+        else return -1;
+    }
+
+    public static LinkedHashMap<Skills, Double> getRecomendedSkills(String username) throws IOException {
+        HashMap<Skills, Double> map = new HashMap<Skills, Double>();
+
+        JsonParser parser = new JsonParser();
+        
+        String response = "";
+ 
+        response = Utils.getRequest("https://api.mojang.com/users/profiles/minecraft/" + username);
+    
+        if(response.startsWith("Error")) {
+            Utils.sendClientMessage(Utils.colorCodes("Error getting data for " + username +". Maybe the player is nicked or there is an invalid API key. Try running /api new."));
+            return null;
+        }
+        JsonObject uuidJson = (JsonObject) parser.parse(response);
+        
+        String uuid = uuidJson.get("id").getAsString();
+
+        response = Utils.getRequest("https://hypixel-api.senither.com/v1/profiles/" + uuid + "/latest?key=8329f5b9-ee7b-43e9-985e-6a6d5f0aa870");
+
+        if(response.startsWith("Error")) {
+            Utils.sendClientMessage(Utils.colorCodes("Error getting data for " + username + ". Maybe the player is nicked or there is an invalid API key. Try running /api new."));
+            return null;
+        }
+
+        JsonObject senitherJson = (JsonObject) parser.parse(response);
+        JsonObject skills = senitherJson.getAsJsonObject("data").getAsJsonObject("skills");
+        if (skills.getAsJsonObject("mining").get("level").getAsDouble() < maxLevel(Skills.MINING)) 
+            map.put(Skills.MINING,(Math.pow(skills.getAsJsonObject("mining").get("level").getAsDouble()* 10, 1.68207448 + skills.getAsJsonObject("mining").get("level").getAsDouble()/100)/1250) - (Math.pow(Math.ceil(skills.getAsJsonObject("mining").get("level").getAsDouble())* 10, 1.68207448 + (Math.floor(skills.getAsJsonObject("mining").get("level").getAsDouble())+1)/100)/1250));
+        if (skills.getAsJsonObject("foraging").get("level").getAsDouble() < maxLevel(Skills.FORAGING)) 
+            map.put(Skills.FORAGING,(Math.pow(skills.getAsJsonObject("foraging").get("level").getAsDouble()* 10, 1.732826 + skills.getAsJsonObject("foraging").get("level").getAsDouble()/100)/1250) - (Math.pow(Math.ceil(skills.getAsJsonObject("foraging").get("level").getAsDouble())* 10, 1.732826 + (Math.floor(skills.getAsJsonObject("foraging").get("level").getAsDouble())+1)/100)/1250));
+        if (skills.getAsJsonObject("enchanting").get("level").getAsDouble() < maxLevel(Skills.ENCHANTING)) 
+            map.put(Skills.ENCHANTING,(Math.pow(skills.getAsJsonObject("enchanting").get("level").getAsDouble()* 10, 1.468976583 + skills.getAsJsonObject("enchanting").get("level").getAsDouble()/100)/1250) - (Math.pow(Math.ceil(skills.getAsJsonObject("enchanting").get("level").getAsDouble())* 10, 1.468976583 + (Math.floor(skills.getAsJsonObject("enchanting").get("level").getAsDouble())+1)/100)/1250));
+        if (skills.getAsJsonObject("farming").get("level").getAsDouble() < maxLevel(Skills.FARMING)) 
+            map.put(Skills.FARMING,(Math.pow(skills.getAsJsonObject("farming").get("level").getAsDouble()* 10, 1.717848139 + skills.getAsJsonObject("farming").get("level").getAsDouble()/100)/1250) - (Math.pow(Math.ceil(skills.getAsJsonObject("farming").get("level").getAsDouble())* 10, 1.717848139 + (Math.floor(skills.getAsJsonObject("farming").get("level").getAsDouble())+1)/100)/1250));
+        if (skills.getAsJsonObject("combat").get("level").getAsDouble() < maxLevel(Skills.COMBAT)) 
+            map.put(Skills.COMBAT,(Math.pow(skills.getAsJsonObject("combat").get("level").getAsDouble()* 10, 1.65797687265 + skills.getAsJsonObject("combat").get("level").getAsDouble()/100)/1250) - (Math.pow(Math.ceil(skills.getAsJsonObject("combat").get("level").getAsDouble())* 10, 1.65797687265 + (Math.floor(skills.getAsJsonObject("combat").get("level").getAsDouble())+1)/100)/1250));
+        if (skills.getAsJsonObject("fishing").get("level").getAsDouble() < maxLevel(Skills.FISHING)) 
+            map.put(Skills.FISHING,(Math.pow(skills.getAsJsonObject("fishing").get("level").getAsDouble()* 10, 1.906418 + skills.getAsJsonObject("fishing").get("level").getAsDouble()/100)/1250) - (Math.pow(Math.ceil(skills.getAsJsonObject("fishing").get("level").getAsDouble())* 10, 1.906418 + (Math.floor(skills.getAsJsonObject("fishing").get("level").getAsDouble())+1)/100)/1250));
+        if (skills.getAsJsonObject("alchemy").get("level").getAsDouble() < maxLevel(Skills.ALCHEMY)) {
+            map.put(Skills.ALCHEMY,(Math.pow(skills.getAsJsonObject("alchemy").get("level").getAsDouble()* 10, 1.5 + skills.getAsJsonObject("alchemy").get("level").getAsDouble()/100)/1250) - (Math.pow(Math.ceil(skills.getAsJsonObject("alchemy").get("level").getAsDouble())* 10, 1.5 + (Math.floor(skills.getAsJsonObject("alchemy").get("level").getAsDouble())+1)/100)/1250));
+        }
+       
+        
+        if (senitherJson.getAsJsonObject("data").getAsJsonObject("dungeons").getAsJsonObject("types").getAsJsonObject("catacombs").get("level").getAsDouble() < maxLevel(Skills.CATACOMBS)) map.put(Skills.CATACOMBS,(Math.pow(Math.floor(senitherJson.getAsJsonObject("data").getAsJsonObject("dungeons").getAsJsonObject("types").getAsJsonObject("catacombs").get("level").getAsDouble()), 4.5)* 0.0002149604615) - (Math.pow(Math.floor(senitherJson.getAsJsonObject("data").getAsJsonObject("dungeons").getAsJsonObject("types").getAsJsonObject("catacombs").get("level").getAsDouble()+1), 4.5)* 0.0002149604615));
+
+        LinkedHashMap<Skills, Double> sortedMap = new LinkedHashMap<Skills, Double>();
+        ArrayList<Double> list = new ArrayList<>();
+        for (Map.Entry<Skills, Double> entry : map.entrySet()) {
+            list.add(entry.getValue());
+        }
+        Collections.sort(list, new Comparator<Double>() {
+            public int compare(Double str, Double str1) {
+                return (str).compareTo(str1);
+            }
+        });
+        for (Double str : list) {
+            for (Entry<Skills, Double> entry : map.entrySet()) {
+                if (entry.getValue().equals(str)) {
+                    sortedMap.put(entry.getKey(), str);
+                }
+            }
+        }
+
+        return sortedMap;
+    }
+
+
+    
+}
