7f13a7b48bd7e2e80970ae3939ed0fb0ff3525d7
Su386yt <83100266+Su386yt@users.noreply.github.com>
2022-09-06 17:09:41 -0700

    Fixed help error message

--- src/main/java/me/partlysanestudios/partlysaneskies/help/Help.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/help/Help.java
@@ -11,36 +11,36 @@ public class Help {
     
     public static void printHelpMessage() {
         try {
-            Main.minecraft.ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(Utils.colorCodes("""
-&3&m-----------------------------------------------------&r
-&b&l&nWelcome to Partly Sane Skies!&r
-Partly Sane Skies is a mod developed by Su386 and FlagMaster. This mod aims to be a quality of life mod for Hypixel Skyblock.
-
- &6> Open the config: 
-    &6> &eDefault """ + Keyboard.getKeyName(KeyInit.helpKey.getKeyCode()) + """
-    &6> &eMost features are turned off by default so to use the mod, you will need to configure the settings
-    &6> &eTo change the keybinding, press Esc, Options, Video Settings, Controls, and scroll down to \"Partly Sane Skies\".
-
- &2> Try out the new Partly Sane Skies Party Manager
-    &2> &aCommand: /pm
-
- &1> Join the discord
-    &1> &9To recieve any future updates
-
- &5> Visit the GitHub
-    &5> 7dAll of the features wouldn't fit in this message, so check out the GitHub to see all of the features.
-&3&m-----------------------------------------------------&r
-Commands:
- > /pss
-    > Brings up the help screen
-
- > /pm, /partymanager
-    > Opens the Party Manager
-
- > /skillup <username>, /su <username> 
-    > Recommends which skill you should upgrade next.
-&3&m-----------------------------------------------------&r
-""")));
+            Main.minecraft.ingameGUI.getChatGUI().printChatMessage(new ChatComponentText(Utils.colorCodes(
+"&3&m-----------------------------------------------------&r"+
+"\n&b&l&nWelcome to Partly Sane Skies!&r"+
+"\nPartly Sane Skies is a mod developed by Su386 and FlagMaster. This mod aims to be a quality of life mod for Hypixel Skyblock."+
+"\n"+
+"\n &6> Open the config: "+
+"\n    &6> &eDefault " + Keyboard.getKeyName(KeyInit.helpKey.getKeyCode())+
+"\n    &6> &eMost features are turned off by default so to use the mod, you will need to configure the settings"+
+"\n    &6> &eTo change the keybinding, press Esc, Options, Video Settings, Controls, and scroll down to \"Partly Sane Skies\"."+
+"\n"+
+"\n &2> Try out the new Partly Sane Skies Party Manager"+
+"\n    &2> &aCommand: /pm"+
+"\n"+
+"\n &1> Join the discord"+
+"\n    &1> &9To recieve any future updates"+
+"\n"+
+"\n &5> Visit the GitHub"+
+"\n    &5> 7dAll of the features wouldn't fit in this message, so check out the GitHub to see all of the features."+
+"\n&3&m-----------------------------------------------------&r"+
+"\nCommands:"+
+"\n > /pss"+
+"\n    > Brings up the help screen"+
+"\n"+
+"\n > /pm, /partymanager"+
+"\n    > Opens the Party Manager"+
+"\n"+
+"\n > /skillup <username>, /su <username> "+
+"\n    > Recommends which skill you should upgrade next."+
+"\n&3&m-----------------------------------------------------&r"
+)));
         }   
         catch(NullPointerException e) {}
         finally {}
