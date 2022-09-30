27cbba14fd28126b620cbe2800e87e764259ec60
Su386yt <83100266+Su386yt@users.noreply.github.com>
2022-09-29 17:37:49 -0700

    Revert "Revert "Refactored keybinds""
    This reverts commit 2fe9d96c709b4839bbb8f4bafb91e52c07f5bfa1.

--- src/main/java/me/partlysanestudios/partlysaneskies/KeyInit.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/KeyInit.java
@@ -2,8 +2,14 @@ package me.partlysanestudios.partlysaneskies;
 
 import org.lwjgl.input.Keyboard;
 
+import me.partlysanestudios.partlysaneskies.dungeons.partymanager.PartyManager;
+import me.partlysanestudios.partlysaneskies.help.Help;
+import me.partlysanestudios.partlysaneskies.utils.Utils;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 
 public final class KeyInit {
     
@@ -38,4 +44,35 @@ public final class KeyInit {
         ClientRegistry.registerKeyBinding(key);
         return key;
     }
+
+    @SubscribeEvent
+    public void checkKeyBinds(KeyInputEvent event) {
+        if(debugKey.isPressed()) {
+            Main.isDebugMode = !Main.isDebugMode;
+            Utils.visPrint("Debug mode: " + Main.isDebugMode);
+            Utils.visPrint(Main.getRegionName());
+            Main.locationBannerDisplay.lastLocationTime = Minecraft.getSystemTime();
+        }
+        if(configKey.isPressed()) {
+            Main.minecraft.displayGuiScreen(Main.config.gui());
+        }
+        if(partyManagerKey.isPressed()) {
+            PartyManager.startPartyManager();
+        }
+        if(helpKey.isPressed()) {
+            Help.printHelpMessage();
+        }
+        if(craftKeybind.isPressed()) {
+            Main.minecraft.thePlayer.sendChatMessage("/craft");
+        }
+        if(petKeybind.isPressed()) {
+            Main.minecraft.thePlayer.sendChatMessage("/pets");
+        }
+        if(wardrobeKeybind.isPressed()) {
+            Main.minecraft.thePlayer.sendChatMessage("/wardrobe");
+        }
+        if(storageKeybind.isPressed()) {
+            Main.minecraft.thePlayer.sendChatMessage("/storage");
+        }
+    } 
 }
