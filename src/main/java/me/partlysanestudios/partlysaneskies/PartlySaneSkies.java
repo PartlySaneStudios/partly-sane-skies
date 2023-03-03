2808a1bebecee482cca9f67efd53c6dd7efdb4f2
Su386yt <83100266+Su386yt@users.noreply.github.com>
2023-03-02 18:33:52 -0800

    Revert "Revert "Merge pull request #117 from Su386yt/lisencing""
    This reverts commit bc9689f01af8ac0167ec9cf54b65cb598c6eae4a.

--- src/main/java/me/partlysanestudios/partlysaneskies/PartlySaneSkies.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/PartlySaneSkies.java
@@ -1,8 +1,22 @@
 /*
  * Partly Sane Skies: A Hypixel Skyblock QOL and Economy mod
- * 
  * Created by Su386#9878 (Su386yt) and FlagMaster#1516 (FlagHater), the Partly Sane Studios team
  * Copyright (C) ©️ Su386 and FlagMaster 2023
+ * This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * 
+ * 
  * 
  * Partly Sane Skies would not be possible with out the help of these projects:
  * Minecraft Forge
