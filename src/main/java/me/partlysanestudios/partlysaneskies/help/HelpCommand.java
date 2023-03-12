b32703ed430ac0eb31275276b9d4f9655787cd5e
Su386yt <83100266+Su386yt@users.noreply.github.com>
2023-03-12 11:21:31 -0700

    Changed the help command to have the right keybind and command to open config

--- src/main/java/me/partlysanestudios/partlysaneskies/help/HelpCommand.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/help/HelpCommand.java
@@ -90,7 +90,7 @@ public class HelpCommand implements ICommand {
                 "\nPartly Sane Skies is a mod developed by Su386 and FlagMaster. This mod aims to be a quality of life mod for Hypixel Skyblock." +
                 "\n" +
                 "\n &6> Open the config: " +
-                "\n    &6> &ePress " + Keyboard.getKeyName(Keybinds.helpKey.getKeyCode()) +
+                "\n    &6> &ePress " + Keyboard.getKeyName(Keybinds.configKey.getKeyCode()) + " or use /pssc" +
                 "\n    &6> &eMost features are turned off by default so to use the mod, you will need to configure the settings" +
                 "\n    &6> &eTo change the keybinding, press Esc, Options, Video Settings, Controls, and scroll down to \"Partly Sane Skies\"." +
                 "\n" +
