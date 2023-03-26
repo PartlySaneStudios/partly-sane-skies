3b35b67fd815ec3d22b692fd7200e2a3ebc852f1
Su386yt <83100266+Su386yt@users.noreply.github.com>
2023-03-26 09:44:41 -0700

    Used architectuary

---
+++ root.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    kotlin("jvm") version "1.6.21" apply false
+    id("cc.polyfrost.multi-version.root")
+    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
+}
+
+preprocess {
+    "1.12.2-forge"(11202, "srg") {
+        "1.8.9-forge"(10809, "srg")
+    }
+}
