624586c187b837c7b6a624b5b6bf5aa15d04eb49
AfkUser <95704481+AfkUserMC@users.noreply.github.com>
2023-04-12 20:07:55 -0400

    Revert command format 2
    Signed-off-by: AfkUser <95704481+AfkUserMC@users.noreply.github.com>

--- src/main/java/me/partlysanestudios/partlysaneskies/help/HelpCommand.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/help/HelpCommand.java
@@ -46,7 +46,7 @@ public class HelpCommand implements ICommand {
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "/pss ";
+        return "Show the Partly Sane Skies help message";
     }
 
     @Override
