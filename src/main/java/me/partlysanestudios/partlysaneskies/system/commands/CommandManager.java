c366f1e82fe29eb2175726221495cc3e34765aef
Su386 <83100266+Su386yt@users.noreply.github.com>
2023-08-02 23:34:54 -0700

    Added license information

--- src/main/java/me/partlysanestudios/partlysaneskies/system/commands/CommandManager.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/system/commands/CommandManager.java
@@ -1,3 +1,8 @@
+//
+// Written by Su386.
+// See LICENSE for copyright and license notices.
+//
+
 package me.partlysanestudios.partlysaneskies.system.commands;
 
 import net.minecraft.command.CommandException;
