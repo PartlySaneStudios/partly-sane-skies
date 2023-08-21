4b689e5877eba09198700ed60663537d561c05b0
FlagHater <106500506+FlagHater@users.noreply.github.com>
2023-08-20 17:51:03 -0700

    Crêpe
    Created the /crêpe command.

--- src/main/java/me/partlysanestudios/partlysaneskies/HelpCommands.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/HelpCommands.java
@@ -61,6 +61,37 @@ public class HelpCommands {
                 }).register();
     }
 
+    public static void registerCrepesCommand() {
+        new PSSCommand("crepes")
+                .addAlias("crêpes")
+                .setDescription("Crepes!")
+                .setRunnable((s, a) -> {
+                    ChatComponentText chatComponent= new ChatComponentText("§0-----------------------------------------------------§0" +
+                            "\n§6Ingredients:" +
+                            "\n" +
+                            "\n§7- 250g of wheat flour" +
+                            "\n§7- 50g of butter" +
+                            "\n§7- 50cl of milk" +
+                            "\n§7- 10cl of water" +
+                            "\n§7- 4 (big) eggs" +
+                            "\n§7- 2 tablespoons of powdered sugar" +
+                            "\n§7- 1 pinch of salt" +
+                            "\n" +
+                            "\n§71) In a large mixing bowl, add the wheat flour, the 2 tablespoons of powdered sugar, and 1 pinch of salt" +
+                            "\n§72) While whisking the bowl, progressively pour in the milk and the water " +
+                            "\n§73) After, add in the (melted) butter and the eggs" +
+                            "\n§74) Leave the bowl in the fridge for 30-45 minutes" +
+                            "\n§75) Look up a tutorial on how to make crêpes on a pan" +
+                            "\n§76) Enjoy your (burnt) crêpes with something like Nutella, sugar, or marmalade" +
+                            "\n" +
+                            "\n§7Source: " +
+                            "\n§5http://www.recettes-bretonnes.fr/crepe-bretonne/recette-crepe.html" +
+                            "\n§0-----------------------------------------------------§0");
+                    chatComponent.getChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,"http://www.recettes-bretonnes.fr/crepe-bretonne/recette-crepe.html"));
+                    Utils.sendClientMessage(chatComponent);
+                }).register();
+
+    }
     static List<String> configAliases = Arrays.asList("conf", "c", "config");
     public static void registerHelpCommand() {
         new PSSCommand("psshelp")
