c95134a553c7f9218854e5ae23a93cd730b1f1ee
Su386 <83100266+Su386yt@users.noreply.github.com>
2023-09-20 10:39:53 -0700

    Added license and fixed Compile time error

--- src/main/java/me/partlysanestudios/partlysaneskies/auctionhouse/menu/AuctionHouseGui.kt
+++ src/main/java/me/partlysanestudios/partlysaneskies/auctionhouse/menu/AuctionHouseGui.kt
@@ -1,3 +1,9 @@
+//
+// Written by Su386.
+// See LICENSE for copyright and license notices.
+//
+
+
 package me.partlysanestudios.partlysaneskies.auctionhouse.menu
 
 import gg.essential.elementa.ElementaVersion
--- src/main/java/me/partlysanestudios/partlysaneskies/system/Keybinds.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/system/Keybinds.java
@@ -5,26 +5,24 @@
 
 package me.partlysanestudios.partlysaneskies.system;
 
-import cc.polyfrost.oneconfig.config.core.OneKeyBind;
-import me.partlysanestudios.partlysaneskies.PartlySaneSkies;
-import me.partlysanestudios.partlysaneskies.WikiArticleOpener;
 import me.partlysanestudios.partlysaneskies.HelpCommands;
+import me.partlysanestudios.partlysaneskies.PartlySaneSkies;
+import me.partlysanestudios.partlysaneskies.PetAlert;
+import me.partlysanestudios.partlysaneskies.WikiArticleOpener;
+import me.partlysanestudios.partlysaneskies.auctionhouse.menu.AuctionHouseGui;
+import me.partlysanestudios.partlysaneskies.dungeons.partymanager.PartyManager;
 import me.partlysanestudios.partlysaneskies.garden.MathematicalHoeRightClicks;
 import me.partlysanestudios.partlysaneskies.utils.StringUtils;
+import me.partlysanestudios.partlysaneskies.utils.Utils;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.event.ClickEvent;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
-import org.lwjgl.input.Keyboard;
-
-import me.partlysanestudios.partlysaneskies.auctionhouse.AhGui;
-import me.partlysanestudios.partlysaneskies.dungeons.partymanager.PartyManager;
-import me.partlysanestudios.partlysaneskies.PetAlert;
-import me.partlysanestudios.partlysaneskies.utils.Utils;
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.event.GuiScreenEvent.KeyboardInputEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
+import org.lwjgl.input.Keyboard;
 
 public final class Keybinds {
 
@@ -78,13 +76,13 @@ public final class Keybinds {
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
-            if (PartlySaneSkies.minecraft.currentScreen instanceof AhGui) {
+            if (PartlySaneSkies.minecraft.currentScreen instanceof AuctionHouseGui || AuctionHouseGui.Companion.isAhGui()) {
                 Utils.clickOnSlot(46);
             }
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
-            if (PartlySaneSkies.minecraft.currentScreen instanceof AhGui) {
+            if (PartlySaneSkies.minecraft.currentScreen instanceof AuctionHouseGui || AuctionHouseGui.Companion.isAhGui()) {
                 Utils.clickOnSlot(53);
             }
         }
