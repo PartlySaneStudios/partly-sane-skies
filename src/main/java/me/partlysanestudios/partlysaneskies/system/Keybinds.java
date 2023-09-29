fa98cce11bb20708cd9ca5a4683f6b03ffaaa282
Su386 <83100266+Su386yt@users.noreply.github.com>
2023-09-29 16:39:22 -0700

    Formatted

--- src/main/java/me/partlysanestudios/partlysaneskies/system/Keybinds.java
+++ src/main/java/me/partlysanestudios/partlysaneskies/system/Keybinds.java
@@ -77,13 +77,15 @@ public final class Keybinds {
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
-            if (PartlySaneSkies.minecraft.currentScreen instanceof AuctionHouseGui || (PartlySaneSkies.minecraft.currentScreen instanceof GuiChest && AuctionHouseGui.Companion.isAhGui(PartlySaneSkies.getSeparateUpperLowerInventories(PartlySaneSkies.minecraft.currentScreen)[0]))) {
+            if (PartlySaneSkies.minecraft.currentScreen instanceof AuctionHouseGui ||
+                    (PartlySaneSkies.minecraft.currentScreen instanceof GuiChest && AuctionHouseGui.Companion.isAhGui(PartlySaneSkies.getSeparateUpperLowerInventories(PartlySaneSkies.minecraft.currentScreen)[0]))) {
                 Utils.clickOnSlot(46);
             }
         }
 
         if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
-            if (PartlySaneSkies.minecraft.currentScreen instanceof AuctionHouseGui || (PartlySaneSkies.minecraft.currentScreen instanceof GuiChest && AuctionHouseGui.Companion.isAhGui(PartlySaneSkies.getSeparateUpperLowerInventories(PartlySaneSkies.minecraft.currentScreen)[0]))) {
+            if (PartlySaneSkies.minecraft.currentScreen instanceof AuctionHouseGui ||
+                    (PartlySaneSkies.minecraft.currentScreen instanceof GuiChest && AuctionHouseGui.Companion.isAhGui(PartlySaneSkies.getSeparateUpperLowerInventories(PartlySaneSkies.minecraft.currentScreen)[0]))) {
                 Utils.clickOnSlot(53);
             }
         }
