5521d335c0931a053df6da350a5430124dafab3a
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2023-10-10 18:57:57 +0200

    NO MORE BANNER PERFORMANCE PROBLEMS!! + also added some license comments

--- src/main/java/me/partlysanestudios/partlysaneskies/garden/endoffarmnotifer/points/Point2d.kt
+++ src/main/java/me/partlysanestudios/partlysaneskies/garden/endoffarmnotifer/points/Point2d.kt
@@ -1,9 +1,10 @@
-package me.partlysanestudios.partlysaneskies.garden.endoffarmnotifer.points
-
 //
-// Written by Su386 and K10a1n15.
+// Written by Su386 and J10a1n15.
 // See LICENSE for copyright and license notices.
 //
+
+package me.partlysanestudios.partlysaneskies.garden.endoffarmnotifer.points
+
 open class Point2d(val x: Double, val y: Double) {
     fun getPointX(): Double {
         return x
--- src/main/java/me/partlysanestudios/partlysaneskies/garden/endoffarmnotifer/points/Point3d.kt
+++ src/main/java/me/partlysanestudios/partlysaneskies/garden/endoffarmnotifer/points/Point3d.kt
@@ -1,6 +1,8 @@
+//
+// Written by Su386 and J10a1n15.
+// See LICENSE for copyright and license notices.
+//
 package me.partlysanestudios.partlysaneskies.garden.endoffarmnotifer.points
-
-import me.partlysanestudios.partlysaneskies.garden.endoffarmnotifer.points.Point2d
 
 open class Point3d(x: Double, y: Double, val z: Double) : Point2d(x, y) {
     fun getPointZ(): Double {
