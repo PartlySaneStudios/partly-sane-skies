b9b8543d2f36aa39d40504611f4b648e509f9cce
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-01-13 01:37:02 -0800

    Fixed license

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/config/keybinds/Keybinds.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/config/keybinds/Keybinds.kt
@@ -1,3 +1,9 @@
+//
+// Written by Su386.
+// See LICENSE for copyright and license notices.
+//
+
+
 package me.partlysanestudios.partlysaneskies.config.keybinds
 
 import me.partlysanestudios.partlysaneskies.PartlySaneSkies
