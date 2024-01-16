e9cf77b5713ea695be2583e69a35e5f3d7547d24
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-01-16 21:56:21 +0100

    Revert "Removed debug key check in ah open"
    This reverts commit 012237df2f9dddb2b4ddb685c04bf178997fb12e.

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/features/economy/auctionhousemenu/AuctionHouseGui.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/economy/auctionhousemenu/AuctionHouseGui.kt
@@ -194,6 +194,9 @@ class AuctionHouseGui(defaultAuctionInventory: IInventory) : WindowScreen(Elemen
                 return
             }
 
+            if (DebugKey.isDebugMode()) {
+                return
+            }
 //            val inventory = MinecraftUtils.getSeparateUpperLowerInventories(event.gui)[0]
 
 //            ChatUtils.sendClientMessage("Opening menu")
