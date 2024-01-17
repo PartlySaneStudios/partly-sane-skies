a99f50aa85d669cb78a5e1e0e996f741ceeeb9a5
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-01-17 11:48:00 -0800

    Fixed

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/render/waypoint/Waypoint.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/render/waypoint/Waypoint.kt
@@ -8,10 +8,10 @@ class Waypoint(
     val position: BlockPos,
     val outlineColor: Color = Color(255, 0, 0, 255),
     val fillColor: Color = Color(255, 127, 127, 127),
-    val showBeam: Boolean = false,
+    val showBeam: Boolean = true,
     val showBlockHighlight: Boolean = true,
-    val showLabel: Boolean = false,
-    val showDistance: Boolean = false
+    val showLabel: Boolean = true,
+    val showDistance: Boolean = true
 ) {
     // Additional properties and methods can be added based on your customization needs
 }
