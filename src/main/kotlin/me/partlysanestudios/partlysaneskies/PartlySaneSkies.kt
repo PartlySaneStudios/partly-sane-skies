548c874e806ff168fb114aeca267251523765419
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-02-23 13:54:06 -0800

    Changed to template string

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/PartlySaneSkies.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/PartlySaneSkies.kt
@@ -147,7 +147,7 @@ class PartlySaneSkies {
 
         val mainMenuRequest =
             Request(
-                "${config.apiUrl}/v1/pss/publicdata?owner=" + getRepoOwner() + "&repo=" + getRepoName() + "&path=/data/main_menu.json",
+                "${config.apiUrl}/v1/pss/publicdata?owner=${getRepoOwner()}&repo=${getRepoName()}&path=/data/main_menu.json",
                 { request: Request? ->
                     CustomMainMenu.setMainMenuInfo(
                         request
