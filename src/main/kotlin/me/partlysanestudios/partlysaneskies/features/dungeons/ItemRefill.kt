fe3c612df8a1f2c1215e5a64f86fc07bf1ad41d4
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-02-26 10:49:41 -0800

    Changed en to entry

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/features/dungeons/ItemRefill.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/dungeons/ItemRefill.kt
@@ -71,12 +71,12 @@ object ItemRefill {
                 itemAmount[item] = countItemInInventory(item)
             }
 
-            for (en in itemAmount.entries) {
-                val maxStackSize = SkyblockDataManager.getItem(en.key)?.getStackSize() ?: 64
-                val itemName = SkyblockDataManager.getItem(en.key)?.name ?: ""
-                if (en.value < maxStackSize) {
-                    ChatUtils.sendClientMessage("Refilling ${maxStackSize - en.value} ${itemName}s...")
-                    PartlySaneSkies.minecraft.thePlayer.sendChatMessage("/gfs ${en.key.lowercase(Locale.getDefault())} ${maxStackSize - en.value}")
+            for (entry in itemAmount.entries) {
+                val maxStackSize = SkyblockDataManager.getItem(entry.key)?.getStackSize() ?: 64
+                val itemName = SkyblockDataManager.getItem(entry.key)?.name ?: ""
+                if (entry.value < maxStackSize) {
+                    ChatUtils.sendClientMessage("Refilling ${maxStackSize - entry.value} ${itemName}s...")
+                    PartlySaneSkies.minecraft.thePlayer.sendChatMessage("/gfs ${entry.key.lowercase(Locale.getDefault())} ${maxStackSize - entry.value}")
                 }
 
                 Thread.sleep(2000)
