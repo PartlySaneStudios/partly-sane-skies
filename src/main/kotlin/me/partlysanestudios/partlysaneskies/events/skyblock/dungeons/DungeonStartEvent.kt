34d2fb231b1787084d026affd76d14fa3dc0a008
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-03-13 15:22:14 -0700

    Fixed dungeon start evemt

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/events/skyblock/dungeons/DungeonStartEvent.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/events/skyblock/dungeons/DungeonStartEvent.kt
@@ -8,11 +8,13 @@ package me.partlysanestudios.partlysaneskies.events.skyblock.dungeons
 
 import me.partlysanestudios.partlysaneskies.events.EventManager
 import me.partlysanestudios.partlysaneskies.utils.ChatUtils.sendClientMessage
+import me.partlysanestudios.partlysaneskies.utils.StringUtils.removeColorCodes
 
 class DungeonStartEvent {
     companion object {
         internal fun onMessageRecieved(functionList: List<EventManager.EventFunction>, formattedMessage: String) {
-            if (formattedMessage.contains("[NPC] §bMort§f: Here, I found this map when I first entered") || formattedMessage.contains("[NPC] §bMort§f: §rHere, I found this map when I first entered")) {
+            val message = formattedMessage.removeColorCodes()
+            if (message.contains("Starting in 1 second.")) {
                 for (function in functionList) {
                     try {
                         function.function.call(function.obj, DungeonStartEvent())
