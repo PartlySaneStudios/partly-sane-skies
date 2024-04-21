aad9594c3850f54185b62cdd72d94ea11c0d0adb
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-04-20 20:26:31 -0700

    Update src/main/kotlin/me/partlysanestudios/partlysaneskies/features/debug/CrystalHollowsGemstoneMapper.kt
    Co-authored-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
    Signed-off-by: Su386 <83100266+Su386yt@users.noreply.github.com>

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/features/debug/CrystalHollowsGemstoneMapper.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/debug/CrystalHollowsGemstoneMapper.kt
@@ -172,7 +172,7 @@ object CrystalHollowsGemstoneMapper {
         val file = File("./config/partly-sane-skies/dumps/gemstone-dump-${formattedDate}.json")
         file.createNewFile()
         file.setWritable(true)
-        // Saves teh data to the file
+        // Saves the data to the file
         val writer = FileWriter(file)
         writer.write(json)
         writer.close()
