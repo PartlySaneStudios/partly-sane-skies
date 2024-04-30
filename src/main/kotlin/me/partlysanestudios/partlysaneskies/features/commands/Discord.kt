ed4b598dd147e6c13815dc5fbf55e72f513c8bce
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-04-30 16:19:22 -0700

    Discord to kt

---
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/commands/Discord.kt
@@ -0,0 +1,43 @@
+//
+// Written by Su386.
+// See LICENSE for copyright and license notices.
+//
+
+package me.partlysanestudios.partlysaneskies.features.commands
+
+import me.partlysanestudios.partlysaneskies.PartlySaneSkies
+import me.partlysanestudios.partlysaneskies.PartlySaneSkies.Companion.discordCode
+import me.partlysanestudios.partlysaneskies.PartlySaneSkies.Companion.minecraft
+import me.partlysanestudios.partlysaneskies.commands.PSSCommand
+import net.minecraft.command.ICommandSender
+import net.minecraft.event.ClickEvent
+import net.minecraft.util.ChatComponentText
+import net.minecraft.util.IChatComponent
+
+object Discord {
+    fun registerDiscordCommand() {
+        PSSCommand("discord")
+            .addAlias("pssdisc")
+            .addAlias("pssd")
+            .addAlias("psdisc")
+            .addAlias("psdiscord")
+            .addAlias("pssdiscord")
+            .addAlias("didcord")
+            .setDescription("Join the Partly Sane Studios Discord Server")
+            .setRunnable { s: ICommandSender?, a: Array<String?>? ->
+                // Creates a new message with the correct text
+                val message: IChatComponent =
+                    ChatComponentText(PartlySaneSkies.CHAT_PREFIX + "§9Join the discord: https://discord.gg/" + discordCode)
+                // Sets the text to be clickable with a link
+                message.chatStyle.setChatClickEvent(
+                    ClickEvent(
+                        ClickEvent.Action.OPEN_URL,
+                        "https://discord.gg/" + discordCode
+                    )
+                )
+                // Prints message
+                minecraft.ingameGUI.chatGUI.printChatMessage(message)
+            }
+            .register()
+    }
+}
