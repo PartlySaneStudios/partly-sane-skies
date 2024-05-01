9d44c6a0994055a58ee42d43c1b02a96b9419679
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-04-30 17:57:07 -0700

    CommandManager to kt

---
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/commands/CommandManager.kt
@@ -0,0 +1,64 @@
+//
+// Written by Su386.
+// See LICENSE for copyright and license notices.
+//
+package me.partlysanestudios.partlysaneskies.commands
+
+import net.minecraft.command.CommandException
+import net.minecraft.command.ICommand
+import net.minecraft.command.ICommandSender
+import net.minecraft.util.BlockPos
+import net.minecraftforge.client.ClientCommandHandler
+
+class CommandManager {
+    fun getCommand(commandName: String): PSSCommand? {
+        return commandList[commandName]
+    }
+
+    companion object {
+        var commandList = HashMap<String, PSSCommand>()
+        fun registerCommand(pssCommand: PSSCommand): ICommand? {
+            if (pssCommand.isRegistered) {
+                return null
+            }
+            val iCommand: ICommand = object : ICommand {
+                override fun getCommandName(): String {
+                    return pssCommand.name
+                }
+
+                override fun getCommandUsage(sender: ICommandSender): String {
+                    return pssCommand.description
+                }
+
+                override fun getCommandAliases(): List<String> {
+                    return pssCommand.aliases
+                }
+
+                @Throws(CommandException::class)
+                override fun processCommand(sender: ICommandSender, args: Array<String>) {
+                    pssCommand.runRunnable(sender, args)
+                }
+
+                override fun canCommandSenderUseCommand(sender: ICommandSender): Boolean {
+                    return true
+                }
+
+                override fun addTabCompletionOptions(sender: ICommandSender, args: Array<String>, pos: BlockPos): List<String> {
+                    return ArrayList()
+                }
+
+                override fun isUsernameIndex(args: Array<String>, index: Int): Boolean {
+                    return false
+                }
+
+                override fun compareTo(o: ICommand): Int {
+                    return this.commandName.compareTo(o.commandName)
+                }
+            }
+            pssCommand.setICommand(iCommand)
+            ClientCommandHandler.instance.registerCommand(iCommand)
+            commandList[pssCommand.name] = pssCommand
+            return iCommand
+        }
+    }
+}
