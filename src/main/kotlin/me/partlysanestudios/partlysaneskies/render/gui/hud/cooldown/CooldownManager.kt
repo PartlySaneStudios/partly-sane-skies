0ee1f5d634bb2f5ad600375432fdcc4154623c67
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-04-30 18:27:33 -0700

    PSSItemRender to kt

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/render/gui/hud/cooldown/CooldownManager.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/render/gui/hud/cooldown/CooldownManager.kt
@@ -6,15 +6,6 @@
 
 package me.partlysanestudios.partlysaneskies.render.gui.hud.cooldown
 
-import gg.essential.elementa.ElementaVersion
-import gg.essential.elementa.components.Window
-import gg.essential.elementa.constraints.CenterConstraint
-import gg.essential.elementa.dsl.percent
-import gg.essential.elementa.dsl.pixels
-import gg.essential.universal.UMatrixStack
-import me.partlysanestudios.partlysaneskies.render.gui.components.PSSHorizontalCooldown
-import net.minecraftforge.client.event.RenderGameOverlayEvent
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.util.*
 
 object CooldownManager {
