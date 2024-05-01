46b4c0da6558eacd57c1e4ca28a7697a585a4704
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-05-01 20:32:13 +0200

    Update src/main/kotlin/me/partlysanestudios/partlysaneskies/commands/PSSCommand.kt
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/commands/PSSCommand.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/commands/PSSCommand.kt
@@ -39,7 +39,7 @@ class PSSCommand(val name: String) {
     }
 
     fun addAlias(vararg aliases: String): PSSCommand {
-        this.aliases.addAll(aliases)
+        return this.aliases.addAll(aliases)
     }
     fun setRunnable(runnable: PSSCommandRunnable): PSSCommand {
         this.runnable = runnable
