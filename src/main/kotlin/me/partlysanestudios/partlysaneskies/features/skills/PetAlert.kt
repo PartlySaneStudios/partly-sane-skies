f161f82c807e8168d74017f9a3a88a6451d66fc2
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-05-13 17:28:37 -0700

    License

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/features/skills/PetAlert.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/skills/PetAlert.kt
@@ -1,3 +1,8 @@
+//
+// Written by Su386.
+// See LICENSE for copyright and license notices.
+//
+
 package me.partlysanestudios.partlysaneskies.features.skills
 
 import gg.essential.elementa.UIComponent
