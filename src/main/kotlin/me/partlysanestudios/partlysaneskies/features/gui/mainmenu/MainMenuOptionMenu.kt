36a229c24a6fb515b2ba6c9f905f5de68cfd8d3a
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-07-01 13:43:23 -0700

    Update MainMenuOptionMenu.kt

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/features/gui/mainmenu/MainMenuOptionMenu.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/gui/mainmenu/MainMenuOptionMenu.kt
@@ -1,3 +1,9 @@
+//
+// Written by Su386.
+// See LICENSE for copyright and license notices.
+//
+
+
 package me.partlysanestudios.partlysaneskies.features.gui.mainmenu
 
 import gg.essential.elementa.ElementaVersion
