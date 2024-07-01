0d762d5c73578d568a6bdc722998f802f9966f6a
Su386 <83100266+Su386yt@users.noreply.github.com>
2024-07-01 08:39:50 -0700

    Update BestiaryLevelUpWebhook.kt

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/features/gui/hud/rngdropbanner/BestiaryLevelUpWebhook.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/gui/hud/rngdropbanner/BestiaryLevelUpWebhook.kt
@@ -46,13 +46,19 @@ object BestiaryLevelUpWebhook: Webhook() {
     fun onChatMessage(event: ClientChatReceivedEvent) {
         val message = event.message.formattedText
 
-        if (!regex.containsMatchIn(message)) {
-            return
+        val (_, mob, oldLevel, newLevel) = regex.find(message)?.destructured ?: return
+        val oldLevelInt = if ("(\\d+)".toRegex().containsMatchIn(oldLevel)) {
+            oldLevel.toIntOrNull() ?: 0
+        } else {
+            oldLevel.romanNumeralToInt()
         }
 
-        val (_, mob, oldLevel, newLevel) = regex.find(message)?.destructured ?: return
-        val oldLevelInt = oldLevel.romanNumeralToInt()
-        val newLevelInt = newLevel.romanNumeralToInt()
+        val newLevelInt = if ("(\\d+)".toRegex().containsMatchIn(newLevel)) {
+            newLevel.toIntOrNull() ?: 0
+        } else {
+            newLevel.romanNumeralToInt()
+        }
+
         if (config.find("multipleOf5")?.asBoolean == true && newLevelInt % 5 == 0) {
             trigger(mob, oldLevelInt, newLevelInt)
         } else if (config.find("multipleOf10")?.asBoolean == true && newLevelInt % 10 == 0) {
