4da22a837469f6eafe3c1927548a4d350f3d6eaa
J10a1n15 <45315647+j10a1n15@users.noreply.github.com>
2024-07-22 22:04:56 +0200

    add license
    Signed-off-by: J10a1n15 <45315647+j10a1n15@users.noreply.github.com>

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/features/skills/SkillUpgradeRecommendation.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/skills/SkillUpgradeRecommendation.kt
@@ -1,3 +1,8 @@
+/*
+ * Written by Su386.
+ * See LICENSE for copyright and license notices.
+ */
+
 package me.partlysanestudios.partlysaneskies.features.skills
 
 import me.partlysanestudios.partlysaneskies.PartlySaneSkies.Companion.minecraft
