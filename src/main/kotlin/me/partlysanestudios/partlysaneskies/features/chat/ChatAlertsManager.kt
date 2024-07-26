cbe6e1d7953faa78b2bcd9df5ee76148a6b13538
Empa <42304516+ItsEmpa@users.noreply.github.com>
2024-07-26 19:55:03 +0200

    su changes
    Co-authored-by: Su386 <83100266+Su386yt@users.noreply.github.com>
    Signed-off-by: Empa <42304516+ItsEmpa@users.noreply.github.com>

--- src/main/kotlin/me/partlysanestudios/partlysaneskies/features/chat/ChatAlertsManager.kt
+++ src/main/kotlin/me/partlysanestudios/partlysaneskies/features/chat/ChatAlertsManager.kt
@@ -1,3 +1,8 @@
+//
+// Written by Su386 and ItsEmpa.
+// See LICENSE for copyright and license notices.
+//
+
 package me.partlysanestudios.partlysaneskies.features.chat
 
 import com.google.gson.Gson
